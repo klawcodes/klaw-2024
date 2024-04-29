662cfb22e3f6031714831d212474b215f7758eb9
klawcodes <codenamezeskil@gmail.com>
2024-04-29 18:11:40 +0700

    change page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import { IncomingMessage } from 'http';
+import { NextApiRequest } from 'next';
 
-export default function RootPage(req: IncomingMessage) {
+export default function RootPage(req: NextApiRequest) {
   return NextResponse.redirect('https://localhost:3000/en');
 }
